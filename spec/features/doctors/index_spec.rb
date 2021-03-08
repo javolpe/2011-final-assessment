97b93d8bd36869d8f6f7f9acde1a80226a44f7f4
Jake Volpe <volpe.jacob@gmail.com>
2021-03-08 09:35:58 -0700

    Add route for doc index page, add built out test for doc index page

---
+++ spec/features/doctors/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Doctor Index Page' do 
+  before :each do 
+    @doc1 = Doctor.create!(name: "James Holden", years_practiced: 7, universtiy: "Wyoming" )
+    @doc2 = Doctor.create!(name: "Prax Meng", years_practiced: 23, universtiy: "Ganymede" )
+    @doc3 = Doctor.create!(name: "Elvi Okoye", years_practiced: 16, universtiy: "Ilus" )
+
+    visit doctors_path
+  end
+
+  it "shows all the doctors and their attributes" do 
+    within("#doctor-#{@doc1.id}") do 
+      expect(page).to have_content("Name: #{@doc1.name}")
+      expect(page).to have_content("Years Practiced: #{@doc1.years_practiced}")
+      expect(page).to have_content("University: #{@doc1.universtiy}")
+    end
+
+    within("#doctor-#{@doc2.id}") do 
+      expect(page).to have_content("Name: #{@doc2.name}")
+      expect(page).to have_content("Years Practiced: #{@doc2.years_practiced}")
+      expect(page).to have_content("University: #{@doc2.universtiy}")
+    end
+
+    within("#doctor-#{@doc3.id}") do 
+      expect(page).to have_content("Name: #{@doc3.name}")
+      expect(page).to have_content("Years Practiced: #{@doc3.years_practiced}")
+      expect(page).to have_content("University: #{@doc3.universtiy}")
+    end
+  end
+end
